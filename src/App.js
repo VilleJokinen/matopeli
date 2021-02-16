a6983c880f3f8f2bfa97e6ebedd0038ab1301d6c
villehaha <villehaha@gmail.com>
2021-02-16 18:11:22 +0200

    Eeppisiä rivejä sivulle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-import SnakeBoard from './SnakeBoard'
+import "./App.css";
+import SnakeBoard from "./SnakeBoard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Matopeli
-      </header>
+      <header className="App-header">Matopeli</header>
       <SnakeBoard />
     </div>
   );
--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -1,28 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState} from "react";
+import "./snakeboard.css";
 
 const SnakeBoard = () => {
-  const height = 10
-  const width = 10
-  var initialRows = []
+  const height = 10;
+  const width = 10;
+  var initialRows = [];
   for (var i = 0; i < height; i++) {
-    initialRows[i] = []
+    initialRows[i] = [];
     for (var j = 0; j < width; j++) {
-      initialRows[i][j] = 'blank'
+      initialRows[i][j] = "blank";
     }
   }
 
-  const [rows, setRows] = useState(initialRows)
-  const displayRows = rows.map(row =>
-    <div className='Snake-row'>
-      {row.map(tile => <div className={tile} />)}
+  const [rows, setRows] = useState(initialRows);
+  const [snake, setSnake] = useState([{x: 4, y: 5}]);
+  const [direction, setDirection] = useState("right");
+
+  const displayRows = rows.map(row => (
+    <div className="Snake-row">
+      {row.map(tile => (
+        <div className={`tile ${tile}`} />
+      ))}
     </div>
-  )
+  ));
 
-return (
-  <div className='Snake-board'>
-    {displayRows}
-  </div>
-  )
-}
+  return <div className="Snake-board">{displayRows}</div>;
+};
 
-export default SnakeBoard
+export default SnakeBoard;
