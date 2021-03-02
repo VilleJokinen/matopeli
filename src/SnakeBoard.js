f0ee2501001b8e843ba025565afde42b68a3f41c
villehaha <villehaha@gmail.com>
2021-03-02 18:10:47 +0200

    jotain hasua

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {useInterval} from "./utils";
 import "./snakeboard.css";
 
 const SnakeBoard = () => {
@@ -23,6 +24,28 @@ const SnakeBoard = () => {
       ))}
     </div>
   ));
+  const displaySnake = () => {
+    const newRows = initialRows;
+    snake.forEach(tile => {
+      newRows[tile.x][tile.y] = "snake";
+    });
+    setRows(newRows);
+  };
+
+  const moveSnake = () => {
+    const newSnake = [];
+    switch (direction) {
+      case "right":
+        newSnake.push({x: snake[0].x, y: (snake[0].y + 1) % width});
+        break;
+      default:
+        break;
+    }
+    setSnake(newSnake);
+    displaySnake();
+  };
+
+  useInterval(moveSnake, 250);
 
   return <div className="Snake-board">{displayRows}</div>;
 };
